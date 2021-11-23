dfdc0379b8453af9e0294041aef8269ef696ec3a
imsuck <49095435+imsuck@users.noreply.github.com>
2021-11-23 12:43:53 +0700

    Use a discord.js bot sample

---
+++ src/commands/utility/ping.js
@@ -0,0 +1,14 @@
+// Ping cmd
+
+module.exports = {
+    command: "ping",
+    name: "Ping",
+    category: "utility",
+    description: "Pong! Shows client and api latency",
+    usage: "ping",
+    acessible: "Members",
+    aliases: [""],
+run: async (client, message) => {
+    message.channel.send(`Command Latency: **${new Date().getTime() - message.createdTimestamp} ms**\nAPI Latency: **${client.ws.ping} ms**`)
+}
+}
